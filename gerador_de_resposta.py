a8e2befa887c3885be0924a93e760622ccf723f4
Carlos André Resende Belo <carlos_andre_resende@yahoo.com.br>
2025-03-28 16:07:49 -0300

    Atualização do código
    Adição de comentários e tratamento de exceções

--- gerador_de_resposta.py
+++ gerador_de_resposta.py
@@ -1,40 +1,60 @@
+# Importação das funções
 from utilidades import descobrir_data, formatar_data, apresentar, exibir_linha
 
+# Inicialização da variável
 iniciais = list()
 
+exibir_linha()
 print('Qual é o assunto do chamado?')
-escolha = int(input('[1] AD\n[2] E-mail\nDigite aqui: '))
 
-nome_completo = str(input('Nome completo: ')).strip().lower()
+# Tratamento de exceções no menu
+try:
+    escolha = int(input('[1] AD\n[2] E-mail\nDigite aqui: '))
 
-nome_separado = nome_completo.split()
+except (ValueError, TypeError):
+    print('Escolha inválida!')
 
-exibir_linha()
-print('Nome formatado: ', end='')
+except KeyboardInterrupt:
+    print('')
+else:
+    nome_completo = str(input('Nome completo: ')).strip().lower()
+    nome_separado = nome_completo.split()
 
-for indice, nome in enumerate(nome_separado):
-    print(nome.capitalize(), end=' ')
-    if indice == 0:
-        iniciais.append(nome[0].upper())
-    else:
-        iniciais.append(nome[0])
-print()
-exibir_linha()
+    # Coleta o primeiro e último nomes
+    primeiro_nome = nome_separado[0]
+    ultimo_nome = nome_separado[-1]
 
-primeiro_nome = nome_separado[0]
-ultimo_nome = nome_separado[-1]
+    # Exibe o nome completo formatado e captura as iniciais
+    exibir_linha()
+    print('Nome formatado: ', end='')
 
-apresentar(descobrir_data())
+    for indice, nome in enumerate(nome_separado):
+        print(nome.capitalize(), end=' ')
+        if indice == 0:
+            iniciais.append(nome[0].upper())
+        else:
+            iniciais.append(nome[0])
+    print()
+    exibir_linha()
 
-if escolha == 1:
-    print(f'Usuário: {primeiro_nome}.{ultimo_nome}')
-elif escolha == 2:
-    print(f'E-mail: {primeiro_nome}.{ultimo_nome}@barbacena.mg.gov.br')
+    # Resposta
+    apresentar(descobrir_data())
 
-print('Senha: ', end='')
-for letra in iniciais:
-    print(letra, end='')
-print(f'#{formatar_data(descobrir_data())}')
-print()
-print('Att...')
-exibir_linha()
+    # Gera o usuário ou endereço de e-mail
+    if escolha == 1:
+        print(f'Usuário: {primeiro_nome}.{ultimo_nome}')
+    elif escolha == 2:
+        print(f'E-mail: {primeiro_nome}.{ultimo_nome}@barbacena.mg.gov.br')
+
+    # Gera a senha
+    print('Senha: ', end='')
+    for letra in iniciais:
+        print(letra, end='')
+    print(f'#{formatar_data(descobrir_data())}')
+    print()
+    print('Att...')
+    print()
+
+finally:
+    print('Programa finalizado!')
+    exibir_linha()
--- utilidades.py
+++ utilidades.py
@@ -1,4 +1,8 @@
 def descobrir_data():
+    """
+    -> Descobre a data atual do sistema
+    :return: data_atual: Data atual do sistema
+    """
     from datetime import datetime
 
     data_atual = datetime.now()
@@ -7,6 +11,11 @@ def descobrir_data():
 
 
 def formatar_data(data):
+    """
+    -> Formata a data para usar somente dia e mês
+    :param data: Data a ser formatada
+    :return: Data formatada
+    """
     from  time import strftime
 
     data_formatada = data.strftime('%d%m')
@@ -15,7 +24,11 @@ def formatar_data(data):
 
 
 def apresentar(data):
-
+    """
+    -> Exibe uma saudação com base no horário
+    :param data: Data de referência
+    :return: Sem retorno
+    """
     print()
     if data.hour < 12:
         print('Bom dia,')
@@ -24,5 +37,8 @@ def apresentar(data):
     print()
 
 def exibir_linha():
+    """
+    -> Exibe uma linha separadora
+    :return: sem retorno
+    """
     print('-' * 60)
-
